f50a38c0f31232833febadbb4521375d82a460ee
ScApeGoAT666 <julitamajer@gmail.com>
2025-12-02 21:01:27 +0100

    sidebar

--- frontend/todo/src/app/app.ts
+++ frontend/todo/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../user/user';
+import { Tags } from '../tags/tags';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, User, Tags],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
